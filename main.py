03957f85baecae820a795d1b2ce926492aceb81a
Daniel Zapata <zdaniel4771@gmail.com>
2025-07-18 20:31:32 -0700

    Added ability to let llm actually call functions now

--- main.py
+++ main.py
@@ -4,10 +4,57 @@ from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 
+from functions import get_files_info
 import schemas
 
+def call_function(function_call_part, verbose=False):
+    function_name = function_call_part.name
+    function_args = function_call_part.args
+
+    functions = {
+        "get_files_info": get_files_info.get_files_info,
+        "get_file_content": get_files_info.get_file_content,
+        "write_file": get_files_info.write_file,
+        "run_python_file": get_files_info.run_python_file
+    }
+
+    if not function_name in functions:
+        print(f"Function called {function_name} does not exist")
+    else:
+        func = functions[function_name]
+
+    if verbose:
+        print(f"Calling function: {function_name}({function_args})")
+    else:
+        print(f" - Calling function: {function_name}()")
+
+    function_args["working_directory"] = "./calculator"
+    result = func(**function_args)
+
+    if not function_name in functions:
+        return types.Content(
+            role="tool",
+            parts=[
+                types.Part.from_function_response(
+                    name=function_name,
+                    response={"error": f"Unknown function: {function_name}"},
+                )
+            ],
+        )
+    else:
+        return types.Content(
+            role="tool",
+            parts=[
+                types.Part.from_function_response(
+                    name=function_name,
+                    response={"result": result},
+                )
+            ],
+        )
+
+
 def main():
-    load_dotenv()
+    _ = load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
 
@@ -48,19 +95,32 @@ def main():
     text = content.text
     prompt_tokens = content.usage_metadata.prompt_token_count
     response_tokens = content.usage_metadata.candidates_token_count
-    function_call_part = content.function_calls
+    function_call_parts = content.function_calls
 
     if len(sys.argv) > 2:
-       if sys.argv[2] == "--verbose":
-            print(f"User prompt: : {user_prompt}\n")
-            print(f"Prompt tokens: {prompt_tokens}")
-            print(f"Response tokens: {response_tokens}")
-       else:
-           print("Error: invalid arguments passed")
+        if sys.argv[2] == "--verbose":
+            if function_call_parts:
+                print(f"User prompt: : {user_prompt}\n")
+                print(f"Prompt tokens: {prompt_tokens}")
+                print(f"Response tokens: {response_tokens}")
+
+                for func in function_call_parts:
+                    try:
+                        function_call_result = call_function(func, True)
+                        response_dict = function_call_result.parts[0].function_response.response
+                        print(f"->\n{response_dict['result']}")
+                    except Exception:
+                        sys.exit(1)
+            else:
+                print(text)
+        else:
+            print("Error: invalid arguments passed")
     else:
-        if function_call_part:
-            for func in function_call_part:
-                print(f"Calling function: {func.name}({func.args})")
+        if function_call_parts:
+            for func in function_call_parts:
+                function_call_result = call_function(func)
+                response_dict = function_call_result.parts[0].function_response.response
+                print(f"->\n{response_dict['result']}")
             print(text)
         else:
             print(text)
